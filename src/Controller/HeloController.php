61f94127c75f04a4b34523bacff987a3a2f29ad1
vagrant <vagrant@debian>
2016-08-08 12:00:56 +0900

    修正

--- src/Controller/HeloController.php
+++ src/Controller/HeloController.php
@@ -5,31 +5,46 @@ use App\Controller\AppController;
  
 class HeloController extends AppController
 {
-	    public function index($a = '', $b = '')
-			{
-				 if ($a == '') {
-					             $this->setAction('err');
-											             return;
-											         }
-							        $this->autoRender = false;
-											        echo "<html><head></head><body>";
-											        echo "<h1>Hello!</h1>";
-															echo "<p>これは、サンプルで作成したページです。</p><p>";
-															 if ($a != '') {
-																             echo " パラメータA: " . $a;
-																						         }
-															        if ($b != '') {
-																				            echo " パラメータB: " . $b;
-																										        }
-															        echo "</p></body></html>";
+	public function index($a = '', $b = '')
+	{
+		$id = $this->request->query('id');
+		$name = $this->request->query('name');
+		$this->set('message', 'your id:' . $id . ', name:' . $name);
+						   
+
+	/*
+			//$this->set('message', 'Hello! this is sample page. ;-)');			
+			$str = $this->request->data('text1');
+			if ($str != null){
+				$str = $this->request->data['text1'];
+				$this->set('message', 'you typed:' . $str);
+			} else {
+				$this->set('message','please type...');
 			}
-			 public function err()
-				     {
-							         $this->autoRender = false;
-											         echo "<html><head></head><body>";
-											         echo "<h1>Hello!</h1>";
-															         echo "<p>パラメータがありませんでした。</p>";
-															         echo "</body></html>";
-																			     }
+	/*	if ($a == '') {
+			$this->setAction('err');
+			return;
+		}
+		$this->autoRender = false;
+		echo "<html><head></head><body>";
+		echo "<h1>Hello!</h1>";
+		echo "<p>これは、サンプルで作成したページです。</p><p>";
+		if ($a != '') {
+			echo " パラメータA: " . $a;
+		}
+		if ($b != '') {
+			echo " パラメータB: " . $b;
+		}
+		echo "</p></body></html>";
+	 */
+	}
+	public function err()
+	{
+		$this->autoRender = false;
+		echo "<html><head></head><body>";
+		echo "<h1>Hello!</h1>";
+		echo "<p>パラメータがありませんでした。</p>";
+		echo "</body></html>";
+	}
 
 }
---
+++ src/Template/Helo/index.ctp
@@ -0,0 +1,17 @@
+<div>
+  <h3>Index Page</h3>
+  <p><?= $message ?></p>
+  <form method="post" action="/samplecake/helo/index">
+  <input type="text" name="text1">
+  <input type="submit">
+  </form>
+</div>
+<!--
+<div>
+    <h3>Index Page</h3>
+    <ul class="side-nav">
+				<li>This is sample.</li>
+			<li>	<?= $message ?></li>
+    </ul>
+</div>
+-->
